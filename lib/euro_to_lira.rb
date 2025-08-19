79fe6caf8e2709db0b0629ced6197f5f249f1a9d
Rafael <rafaelmerlotto@gmail.com>
2025-08-19 06:15:04 +0200

    Initial commit

---
+++ lib/euro_to_lira.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "euro_to_lira/version"
+
+module EuroToLira
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/euro_to_lira/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EuroToLira
+  VERSION = "0.1.0"
+end
