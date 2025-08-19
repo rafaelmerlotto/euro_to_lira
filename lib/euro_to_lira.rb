d2b2370719b4bc718f1a7346c170fe7eeaf208e1
Rafael <rafaelmerlotto@gmail.com>
2025-08-19 06:24:52 +0200

    Add module EuroToLira

--- lib/euro_to_lira.rb
+++ lib/euro_to_lira.rb
@@ -4,5 +4,17 @@ require_relative "euro_to_lira/version"
 
 module EuroToLira
   class Error < StandardError; end
-  # Your code goes here...
+  
+  FIXED_RATE = 1936.27
+
+  def self.to_lira(amount_eur)
+    raise ArgumentError, 'Importo non valido' unless amount_eur.is_a?(Numeric)
+    (amount_eur * FIXED_RATE).round(2)
+  end
+
+  def self.to_euro(amount_lira)
+    raise ArgumentError, "Importo non valido" unless amount_lira.is_a?(Numeric)
+    (amount_lira / FIXED_RATE).round(2)
+  end
+
 end
