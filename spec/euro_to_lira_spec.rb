e24fc354d1c908b58d50c89e14c9cc82652fe93b
Rafael <rafaelmerlotto@gmail.com>
2025-08-20 21:55:57 +0200

    Add some tests rspec

--- spec/euro_to_lira_spec.rb
+++ spec/euro_to_lira_spec.rb
@@ -5,8 +5,22 @@ RSpec.describe EuroToLira do
     expect(EuroToLira::VERSION).not_to be nil
   end
 
-  it "converte 1 EUR in 1936.27 lire" do
+  it "convert 1 EUR in 1936.27 lire" do
     expect(EuroToLira.to_lira(1)).to eq(1936.27)
+  end
+
+  it "Convert 1936.27 lire in 1 EUR" do
+    expect(EuroToLira.to_euro(1936.27)).to eq(1)
+  end
+
+  it "returns a value for conversion" do
+    expect(EuroToLira.to_lira(1)).not_to be_nil
+    expect(EuroToLira.to_euro(1)).not_to be_nil
+  end
+
+  it "check if value is numeric" do
+    expect(EuroToLira.to_lira(1)).to be_a(Numeric)
+    expect(EuroToLira.to_euro(1)).to be_a(Numeric)
   end
 
 end
