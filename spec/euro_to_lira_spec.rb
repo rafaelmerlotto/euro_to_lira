24ff97b3b4189f16355cb69638cca6383a7f8993
Rafael <rafaelmerlotto@gmail.com>
2025-08-20 22:04:30 +0200

    Improve test suite

--- spec/euro_to_lira_spec.rb
+++ spec/euro_to_lira_spec.rb
@@ -4,23 +4,38 @@ RSpec.describe EuroToLira do
   it "has a version number" do
     expect(EuroToLira::VERSION).not_to be nil
   end
+  
+  describe ".to_lire" do
+    it "convert 1 EUR in 1936.27 lire" do
+      expect(EuroToLira.to_lira(1)).to eq(1936.27)
+    end
 
-  it "convert 1 EUR in 1936.27 lire" do
-    expect(EuroToLira.to_lira(1)).to eq(1936.27)
+    it "returns a value for conversion" do
+      expect(EuroToLira.to_lira(1)).not_to be_nil
+    end
+
+    it "check if value is numeric" do
+      expect(EuroToLira.to_lira(1)).to be_a(Numeric)
+    end
   end
 
-  it "Convert 1936.27 lire in 1 EUR" do
-    expect(EuroToLira.to_euro(1936.27)).to eq(1)
-  end
+  describe ".to_euro" do
+    it "Convert 1936.27 lire in 1 EUR" do
+      expect(EuroToLira.to_euro(1936.27)).to eq(1)
+    end
 
-  it "returns a value for conversion" do
-    expect(EuroToLira.to_lira(1)).not_to be_nil
-    expect(EuroToLira.to_euro(1)).not_to be_nil
+    it "returns a value for conversion" do
+      expect(EuroToLira.to_euro(1)).not_to be_nil
+    end
+  
+    it "check if value is numeric" do
+      expect(EuroToLira.to_euro(1)).to be_a(Numeric)
+    end
   end
+  
 
-  it "check if value is numeric" do
-    expect(EuroToLira.to_lira(1)).to be_a(Numeric)
-    expect(EuroToLira.to_euro(1)).to be_a(Numeric)
-  end
+  
+
+  
 
 end
