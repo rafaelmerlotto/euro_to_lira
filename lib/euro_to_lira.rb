1b61a36eec2ddc159ba26d5e732aece5ab3b099d
Rafael <rafaelmerlotto@gmail.com>
2025-08-21 22:02:05 +0200

    Fix ArgumentError output

--- lib/euro_to_lira.rb
+++ lib/euro_to_lira.rb
@@ -8,12 +8,12 @@ module EuroToLira
   FIXED_RATE = 1936.27
 
   def self.to_lira(amount_eur)
-    raise ArgumentError, 'Importo non valido' unless amount_eur.is_a?(Numeric)
+    raise ArgumentError, 'Value must be numeric' unless amount_eur.is_a?(Numeric)
     (amount_eur * FIXED_RATE).round(2)
   end
 
   def self.to_euro(amount_lira)
-    raise ArgumentError, "Importo non valido" unless amount_lira.is_a?(Numeric)
+    raise ArgumentError, "Value must be numeric" unless amount_lira.is_a?(Numeric)
     (amount_lira / FIXED_RATE).round(2)
   end
 
